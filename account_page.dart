9fdb3764fd797944617a61d21743715258360b82
murara <41866366+Kaori223@users.noreply.github.com>
2021-12-05 14:05:02 +0900

    Add files via upload

---
+++ account_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccoutPage extends StatefulWidget {
+  const AccoutPage({Key? key}) : super(key: key);
+
+  @override
+  _AccoutPageState createState() => _AccoutPageState();
+}
+
+class _AccoutPageState extends State<AccoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
